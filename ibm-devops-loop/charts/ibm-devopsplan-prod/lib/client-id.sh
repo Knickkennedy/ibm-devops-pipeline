99ab93e4ae7b76b0a85e873ab52cc059f08b400b
Knicholas Kennedy <knicholas.kennedy@ibm.com>
2025-07-15 16:34:01 -0400

    Added pvc override
    Signed-off-by: Knicholas Kennedy <knicholas.kennedy@ibm.com>

--- ibm-devops-loop/charts/ibm-devopsplan-prod/lib/client-id.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -si $CAFILE "$KEYCLOAK_URL/admin/realms/$REALM_NAME/clients?clientId=$CLIENT_ID" \
- -H 'Accept: application/json' \
- -H "Authorization: Bearer $ADMIN_TOKEN" |
- sed -r -n 's/.*"id":"([^"]*)","clientId".*/\1/p'
